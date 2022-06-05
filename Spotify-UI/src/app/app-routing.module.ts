cc7fe51c36e9ae5f429b13a5c10d9c4aa736fbf6
moulid15 <moulid15@gmail.com>
2022-06-05 12:43:29 -0700

    "trying frontend login"

--- Spotify-UI/src/app/app-routing.module.ts
+++ Spotify-UI/src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './comp/login/login.component';
+import { HomeComponent } from './comp/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path:'',component:LoginComponent},{path:'home',component:HomeComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
